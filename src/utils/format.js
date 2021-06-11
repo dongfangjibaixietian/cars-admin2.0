51354f34dcf7def3fa5d99b874954a6c40c53b34
xietian <2475903968@qq.com>
2021-06-11 17:31:58 +0800

    2021/6/11

--- src/utils/format.js
+++ src/utils/format.js
@@ -4,7 +4,7 @@
  * 格式化请求数据的参数 
  * keyData：form_data
  * requestData：请求的数据
- * 
+ * 这是对一个空的JSON对象复制另外的JSON对象的方法
  * */
 export function formatRequestData(keyData, requestData){
     const data = {}
@@ -15,3 +15,37 @@ export function formatRequestData(keyData, requestData){
     }
     return data;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
